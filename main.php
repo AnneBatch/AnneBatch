ebdbdb710241b89fe117f040fd1e7475bc6fb2e6
AnneBatch <79485715+AnneBatch@users.noreply.github.com>
2021-02-22 20:06:43 -0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+
+require_once('database.php');
+require_once('signup_class.php');
+
+$users = new SignUp('localhost', 'database', 'root', 'password');
+$users->setName($_POST['username']);
+$users->setEmail($_POST['email']);
+$users->signUpPage('signup');
+$users->setPassword($_POST['password'], $_POST['password2']);
+$users->existingName();
+$users->existingEmail();
+$users->isDefinedFields();
+$users->ValidateNameLength();
+$users->ValidateEmail();
+$users->addUser();
